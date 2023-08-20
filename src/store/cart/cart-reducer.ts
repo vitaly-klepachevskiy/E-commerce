bfdc7086a2850c8d32779a95a7dea0af31cdf90d
Vitaly Klepachevskiy <vitaly.klepachevskiy@gmail.com>
2023-08-20 16:10:44 +0300

    Web: cart reducer is typed

---
+++ src/store/cart/cart-reducer.ts
@@ -0,0 +1,34 @@
+import { AnyAction } from 'redux';
+import { CartItem } from './cart-types';
+import { setIsCartOpen, setCartItems } from './cart-action';
+
+export type CartState = {
+  isCartOpen: boolean;
+  cartItems: CartItem[];
+};
+
+const CART_INITIAL_STATE: CartState = {
+  isCartOpen: false,
+  cartItems: [],
+};
+
+export const cartReducer = (
+  state = CART_INITIAL_STATE,
+  action: AnyAction
+): CartState => {
+  if (setIsCartOpen.match(action)) {
+    return {
+      ...state,
+      isCartOpen: action.payload,
+    };
+  }
+
+  if (setCartItems.match(action)) {
+    return {
+      ...state,
+      cartItems: action.payload,
+    };
+  }
+
+  return state;
+};
